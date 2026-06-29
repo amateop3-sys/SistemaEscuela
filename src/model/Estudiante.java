1a85fcc2350ea4d92e1acd4502a897287644080e
amateop3-sys <amateop3@miumg.edu.gt>
2026-06-29 01:14:43 -0600

    primera parte, estudiantes

---
+++ src/model/Estudiante.java
@@ -0,0 +1,79 @@
+package model;
+
+public class Estudiante extends Personas {
+	
+	
+	private String carnet;
+	private String grado;
+	private String seccion;
+	private boolean activo;
+	
+	
+	// constructor vacio
+	public Estudiante() {
+	
+	}
+
+	
+
+	// constructor con atributos
+	public Estudiante(String carnet, String grado, String seccion, boolean activo) {
+		super();
+		this.carnet = carnet;
+		this.grado = grado;
+		this.seccion = seccion;
+		this.activo = activo;
+	}
+
+
+
+	// getters and setters
+	public String getCarnet() {
+		return carnet;
+	}
+
+	public void setCarnet(String carnet) {
+		this.carnet = carnet;
+	}
+
+	public String getGrado() {
+		return grado;
+	}
+
+	public void setGrado(String grado) {
+		this.grado = grado;
+	}
+
+	public String getSeccion() {
+		return seccion;
+	}
+
+	public void setSeccion(String seccion) {
+		this.seccion = seccion;
+	}
+
+	public boolean isActivo() {
+		return activo;
+	}
+
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
--- src/model/Personas.java
+++ src/model/Personas.java
@@ -4,15 +4,118 @@ public class Personas {
 	
 
 	
+	private String nombre;
+	private String apellidos;
+	private String dpi;
+	private String telefono;
+	private String direccion;
+	private int edad;
+	private String genero;
+	
+	// constructor vacio
+	public Personas() {
+		
+	}
 	
 	
-	
-	
-	
-	
-	
-	
-	
+	// constructor lleno
+	public Personas(String nombre, String apellidos, String dpi, String telefono, String direccion, int edad,
+			String genero) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.dpi = dpi;
+		this.telefono = telefono;
+		this.direccion = direccion;
+		this.edad = edad;
+		this.genero = genero;
+	}
+
 	
 
+	// getters and setters
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+
+	public String getDpi() {
+		return dpi;
+	}
+
+
+	public void setDpi(String dpi) {
+		this.dpi = dpi;
+	}
+
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+
+	public int getEdad() {
+		return edad;
+	}
+
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+
+	public String getGenero() {
+		return genero;
+	}
+
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	
+	// metodo mostrar informacion
+	public void mostrarInformacion() {
+		
+		System.out.println("Nombre:"+nombre);		
+		System.out.println("Apellidos:"+apellidos);
+		System.out.println("DPI:"+dpi);
+		System.out.println("Telefono:"+telefono);
+		System.out.println("Direccion:"+direccion);
+		System.out.println("Edad:"+edad);
+		System.out.println("Genero:"+genero);
+		
+	}
+	
+	
 }
